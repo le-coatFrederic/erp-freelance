539bb854f8a399b7cb222d98c7e031fb82eb34bc
le-coatFrederic <lecoatfred@gmail.com>
2026-01-12 14:19:48 +0100

    gitignore

---
+++ backend/src/main/java/com/fredlecoat/erp_freelance/ErpFreelanceApplication.java
@@ -0,0 +1,13 @@
+package com.fredlecoat.erp_freelance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpFreelanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpFreelanceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fredlecoat/erp_freelance/ErpFreelanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredlecoat.erp_freelance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpFreelanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
