e624eb5a70da0b7e1a8f1b3d794e83efde659c59
le-coatFrederic <lecoatfred@gmail.com>
2026-01-14 15:37:16 +0100

    correction backend

--- backend/src/main/java/com/fredlecoat/erp_freelance/domain/services/ContactService.java
+++ backend/src/main/java/com/fredlecoat/erp_freelance/domain/services/ContactService.java
@@ -2,6 +2,6 @@ package com.fredlecoat.erp_freelance.domain.services;
 
 import com.fredlecoat.erp_freelance.domain.entities.ContactEntity;
 
-public interface ContactServiceImpl extends EntityCrudService<ContactEntity> {
+public interface ContactService extends EntityCrudService<ContactEntity> {
 
 }
