ebc5f4cb931585a4e1b385c9f6addc9d59f39bd0
le-coatFrederic <lecoatfred@gmail.com>
2026-01-14 16:38:09 +0100

    Modification dto contact

--- backend/src/main/java/com/fredlecoat/erp_freelance/domain/entities/dtos/ContactTotalResponse.java
+++ backend/src/main/java/com/fredlecoat/erp_freelance/domain/entities/dtos/ContactTotalResponse.java
@@ -2,8 +2,8 @@ package com.fredlecoat.erp_freelance.domain.entities.dtos;
 
 public record ContactTotalResponse(
     Long id,
-    String firstName,
-    String lastName,
+    String firstname,
+    String lastname,
     String job,
     String email,
     String linkedin,
