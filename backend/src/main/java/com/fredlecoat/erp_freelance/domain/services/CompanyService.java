169548351d3d5582dd5edfc32a51cf2d13a8eca1
le-coatFrederic <lecoatfred@gmail.com>
2026-01-14 15:29:20 +0100

    correction backend

--- backend/src/main/java/com/fredlecoat/erp_freelance/domain/services/CompanyService.java
+++ backend/src/main/java/com/fredlecoat/erp_freelance/domain/services/CompanyService.java
@@ -2,7 +2,5 @@ package com.fredlecoat.erp_freelance.domain.services;
 
 import com.fredlecoat.erp_freelance.domain.entities.CompanyEntity;
 
-import java.util.List;
-
 public interface CompanyService extends EntityCrudService<CompanyEntity> {
 }
